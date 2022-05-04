b20c8d3ea50d8e8e0699bb0974834e6766c71c47
SantiagoPMendez <santiago.mendez.ordonez@gmail.com>
2022-05-04 01:25:32 -0500

    1st commit

---
+++ FacturaIonic/src/main/java/edu/ucacue/model/Persona.java
@@ -0,0 +1,105 @@
+package edu.ucacue.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "persona")
+public class Persona implements Serializable {
+	
+	private static final long serialVersionUID = 1607830177598686701L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(unique = true)
+	private String cedula;
+	
+	@Column(nullable = false)
+	private String nombre;
+	private String apellido;
+	private String telefono;
+	private String correo;
+	private String coordenadas;
+	
+	public Persona() {		
+	}
+
+	public Persona(int id, String cedula, String nombre, String apellido, String telefono, String correo,
+			String coordenadas) {
+		super();
+		this.id = id;
+		this.cedula = cedula;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.telefono = telefono;
+		this.correo = correo;
+		this.coordenadas = coordenadas;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public String getCoordenadas() {
+		return coordenadas;
+	}
+
+	public void setCoordenadas(String coordenadas) {
+		this.coordenadas = coordenadas;
+	}
+
+	
+}
+	
